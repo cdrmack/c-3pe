e13f65f9420df3806e0e2a3f064974a43be0083e
cdrmack <git.pynvt@passmail.net>
2024-12-04 16:09:12 +0100

    improve layout, add text fields

--- main.c
+++ main.c
@@ -12,21 +12,35 @@ static void activate(GtkApplication *app, gpointer user_data)
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
 
-    GtkWidget *button = gtk_button_new_with_label("calculate");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_result), NULL);
-
     GtkWidget *worst = gtk_label_new("worst");
     GtkWidget *likely = gtk_label_new("likely");
     GtkWidget *best = gtk_label_new("best");
 
-    GtkWidget *h_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
-    gtk_box_set_homogeneous(GTK_BOX(h_box), true);
-    gtk_box_append(GTK_BOX(h_box), worst);
-    gtk_box_append(GTK_BOX(h_box), likely);
-    gtk_box_append(GTK_BOX(h_box), best);
+    GtkWidget *labels_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_set_homogeneous(GTK_BOX(labels_box), true);
+    gtk_box_append(GTK_BOX(labels_box), worst);
+    gtk_box_append(GTK_BOX(labels_box), likely);
+    gtk_box_append(GTK_BOX(labels_box), best);
+
+    GtkWidget *worst_input = gtk_entry_new();
+    GtkWidget *likely_input = gtk_entry_new();
+    GtkWidget *best_input = gtk_entry_new();
+
+    GtkWidget *input_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+    gtk_box_set_homogeneous(GTK_BOX(input_box), true);
+    gtk_box_append(GTK_BOX(input_box), worst_input);
+    gtk_box_append(GTK_BOX(input_box), likely_input);
+    gtk_box_append(GTK_BOX(input_box), best_input);
+
+    GtkWidget *empty = gtk_label_new("");
+
+    GtkWidget *button = gtk_button_new_with_label("calculate");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_result), NULL);
 
     GtkWidget *v_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-    gtk_box_append(GTK_BOX(v_box), h_box);
+    gtk_box_append(GTK_BOX(v_box), labels_box);
+    gtk_box_append(GTK_BOX(v_box), input_box);
+    gtk_box_append(GTK_BOX(v_box), empty);
     gtk_box_append(GTK_BOX(v_box), button);
 
     gtk_widget_set_margin_start(v_box, 10);
